d59915c731a3b01245c0374ccf30da919a8049b2
Marcin <gonzur75@gmail.com>
2022-08-05 22:41:03 +0200

    configure db using dj_databse_url for applay 12 factor for db conectiion

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 import environ
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,6 +86,11 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASE_URL = os.environ.get('DB_CONNECTION_STRING')
+
+db_config = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=False)
+DATABASES['default'].update(db_config)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
