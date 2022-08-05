72d424caa1f666456e185772d8eaeac94e159da6
Marcin <gonzur75@gmail.com>
2022-08-05 22:07:46 +0200

    properly configured env acording to 12-factorapp

--- config/settings.py
+++ config/settings.py
@@ -11,13 +11,14 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+
 import environ
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(os.path.join(BASE_DIR, 'env' '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, 'env/' '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -26,7 +27,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, 'env' '.env'))
 SECRET_KEY = os.environ.get('DJ_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DJ_DEBUG', False))
+
 
 ALLOWED_HOSTS = []
 
